02be69b2b7c8f948f081adde22cb40bf9ba43575
Cesar Baltazar <cesarabc7@gmail.com>
2018-09-24 16:25:47 -0500

    Se agregó el método findByUltimoEstadoId para obtener todos los envíos masivos

--- service-externa/src/main/java/com/exact/service/externa/dao/IEnvioMasivoDao.java
+++ service-externa/src/main/java/com/exact/service/externa/dao/IEnvioMasivoDao.java
@@ -9,4 +9,9 @@ public interface IEnvioMasivoDao extends CrudRepository<EnvioMasivo, Long> {
 
 	@Query(value="SELECT TOP 1 masivo_autogenerado FROM envio_masivo ORDER BY envio_id DESC ", nativeQuery=true)
 	public String getMaxMasivoAutogenerado();
+	
+	@Query("FROM Envio e WHERE e.masivoAutogenerado IS NOT NULL AND e IN (SELECT d.envio FROM Documento d WHERE d IN (SELECT sd.documento "
+			+ "FROM SeguimientoDocumento sd WHERE sd.id = (SELECT MAX(sd2.id) FROM SeguimientoDocumento sd2 "
+			+ "WHERE sd2.documento.id = d.id) AND sd.estadoDocumento.id = ?1))")
+	public Iterable<EnvioMasivo> findByUltimoEstadoId(Long ultimoEstadoId);
 }
