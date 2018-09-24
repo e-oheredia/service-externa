df6b02546c951c71c022a3a9bb0610126a0f6ccc
Cesar Baltazar <cesarabc7@gmail.com>
2018-09-24 16:28:34 -0500

    Se definió el método listarEnviosMasivosCreados

--- service-externa/src/main/java/com/exact/service/externa/service/interfaces/IEnvioMasivoService.java
+++ service-externa/src/main/java/com/exact/service/externa/service/interfaces/IEnvioMasivoService.java
@@ -2,10 +2,13 @@ package com.exact.service.externa.service.interfaces;
 
 import java.io.IOException;
 
+import org.apache.http.client.ClientProtocolException;
+import org.json.JSONException;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.exact.service.externa.entity.EnvioMasivo;
 
 public interface IEnvioMasivoService {
 	EnvioMasivo registrarEnvioMasivo(EnvioMasivo envioMasivo, Long idUsuario, MultipartFile multipartFile) throws IOException;
+	Iterable<EnvioMasivo> listarEnviosMasivosCreados() throws ClientProtocolException, IOException, JSONException;
 }
