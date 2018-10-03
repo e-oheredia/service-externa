eb45856f69f61e93a4a988487d88ca571a9573ad
RonaldSantosR <rsantos@exact.com.pe>
2018-10-03 15:12:58 -0500

    commit tokenExternaValid

--- service-externa/src/main/java/com/exact/service/externa/controller/TokenController.java
+++ service-externa/src/main/java/com/exact/service/externa/controller/TokenController.java
@@ -1,31 +1,25 @@
 package com.exact.service.externa.controller;
 
 
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-import java.util.stream.StreamSupport;
 
-import org.apache.http.HttpEntity;
+
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.util.EntityUtils;
-import org.json.JSONArray;
-import org.json.JSONObject;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpHeaders;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
-import com.exact.service.externa.entity.Documento;
+
 import com.exact.service.externa.request.IRequester;
-import com.exact.service.externa.utils.CommonUtils;
+
 
 
 @RestController
