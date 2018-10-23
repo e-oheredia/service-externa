8814d36630648ef884ac1ccba6140f94071de554
usuario <usuario@DESKTOP-CE6QM64>
2018-10-23 16:24:08 -0500

    Many to Many Proveedor - Plazo_Distribucion

--- service-externa/src/main/java/com/exact/service/externa/entity/Proveedor.java
+++ service-externa/src/main/java/com/exact/service/externa/entity/Proveedor.java
@@ -1,12 +1,19 @@
 package com.exact.service.externa.entity;
 
 import java.io.Serializable;
+import java.util.List;
+import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -21,8 +28,21 @@ public class Proveedor implements Serializable{
 	@Column(name="nombre",nullable=false)
 	private String nombre;
 	
+	@ManyToMany(fetch=FetchType.LAZY, cascade = CascadeType.ALL)
+	@JoinTable(name="proveedor_plazo_distribucion", joinColumns = { @JoinColumn(name = "proveedor_id") },
+    inverseJoinColumns = { @JoinColumn(name = "plazo_distribucion_id") })
+	private Set<PlazoDistribucion> plazosDistribucion;
 	
-	
+	public Set<PlazoDistribucion> getPlazosDistribucion() {
+		return plazosDistribucion;
+	}
+
+	public void setPlazosDistribucion(Set<PlazoDistribucion> plazosDistribucion) {
+		this.plazosDistribucion = plazosDistribucion;
+	}
+
+
+
 	public Long getId() {
 		return id;
 	}
