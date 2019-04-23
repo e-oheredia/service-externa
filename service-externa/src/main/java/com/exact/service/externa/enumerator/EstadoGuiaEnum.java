3e57720eda68ce55ee94a5f620b040fca68cda10
usuario <ymamani@exact.com.pe>
2019-04-23 11:34:39 -0500

    commit -m "añadí enumerador"

--- service-externa/src/main/java/com/exact/service/externa/enumerator/EstadoGuiaEnum.java
+++ service-externa/src/main/java/com/exact/service/externa/enumerator/EstadoGuiaEnum.java
@@ -3,6 +3,7 @@ package com.exact.service.externa.enumerator;
 public class  EstadoGuiaEnum {
 	public static final Long GUIA_CREADO = 1L; 
 	public static final Long GUIA_ENVIADO = 2L; 
-	public static final Long GUIA_DESCARGADO = 3L; 
-	public static final Long GUIA_CERRADO = 4L; 
+	public static final Long GUIA_DESCARGADO = 3L;
+	public static final Long GUIA_COMPLETA = 4L;
+	public static final Long GUIA_CERRADO = 5L; 
 }
