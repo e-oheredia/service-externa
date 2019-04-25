13c88c2ded461d053fee19fe92e39a1b4e9120fa
usuario <ymamani@exact.com.pe>
2019-04-25 14:54:52 -0500

    commit -m "Modificar MOTIVOESTADOS"

---
+++ service-externa/src/main/java/com/exact/service/externa/controller/MotivoEstadoController.java
@@ -0,0 +1,35 @@
+package com.exact.service.externa.controller;
+
+import java.io.IOException;
+import java.util.Map;
+
+import org.apache.http.client.ClientProtocolException;
+import org.json.JSONException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.exact.service.externa.entity.MotivoEstado;
+import com.exact.service.externa.service.interfaces.IDepartamentoService;
+import com.exact.service.externa.service.interfaces.IMotivoEstadoService;
+
+@RestController
+@RequestMapping("/motivosestados")
+public class MotivoEstadoController {
+
+	@Autowired
+	IMotivoEstadoService motivoestado;
+	
+	@GetMapping()
+	public ResponseEntity<Iterable<MotivoEstado>> listarMotivos() throws ClientProtocolException, IOException, JSONException{
+		return new ResponseEntity<Iterable<MotivoEstado>>(motivoestado.listarMotivos() , HttpStatus.OK);
+	}
+	
+	
+	
+	
+}
