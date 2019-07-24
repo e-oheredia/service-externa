f1d699124858e597075ded5eaaeac65a38c162fc
usuario <ymamani@exact.com.pe>
2019-07-24 15:46:04 -0500

    commit -m "subir cambios de model"

--- service-externa/src/main/java/com/exact/service/externa/service/classes/TipoSeguridadService.java
+++ service-externa/src/main/java/com/exact/service/externa/service/classes/TipoSeguridadService.java
@@ -7,8 +7,10 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.exact.service.externa.converter.convertertiposeguridad;
 import com.exact.service.externa.dao.ITipoSeguridadDao;
 import com.exact.service.externa.entity.TipoSeguridad;
+import com.exact.service.externa.model.MTipoSeguridad;
 import com.exact.service.externa.service.interfaces.ITipoSeguridadService;
 
 @Service
@@ -17,25 +19,32 @@ public class TipoSeguridadService implements ITipoSeguridadService {
 	@Autowired
 	ITipoSeguridadDao tipoSeguridadDao;
 	
+	@Autowired
+	convertertiposeguridad converter;
+	
 	@Override
 	public Iterable<TipoSeguridad> listarAll() {
 		return tipoSeguridadDao.findAll();
 	}
 
 	@Override
-	public TipoSeguridad guardar(TipoSeguridad tiposeguridad) {
+	public MTipoSeguridad guardar(TipoSeguridad tiposeguridad) {
 		if(tiposeguridad.getNombre()==null) {
 			return null;
 		}
-		return tipoSeguridadDao.save(tiposeguridad);
+		TipoSeguridad tiposeguridadsave= tipoSeguridadDao.save(tiposeguridad);
+		MTipoSeguridad mtipo = new MTipoSeguridad(tiposeguridadsave);
+		return mtipo;
+		
 	}
 
 	@Override
-	public Iterable<TipoSeguridad> listarTipoSeguridadActivos() {
+	public Iterable<MTipoSeguridad> listarTipoSeguridadActivos() {
 		Iterable<TipoSeguridad> tipoSeguridadBD = tipoSeguridadDao.findAll();
 		List<TipoSeguridad> tipoSeguridadlst = StreamSupport.stream(tipoSeguridadBD.spliterator(), false).collect(Collectors.toList());
 		tipoSeguridadlst.removeIf(tiposeguridad -> !tiposeguridad.isActivo());
-		return tipoSeguridadlst;
+		List<MTipoSeguridad> tiposeguridadlist=converter.convertirLista(tipoSeguridadlst);
+		return tiposeguridadlist;
 	}
 
 }
